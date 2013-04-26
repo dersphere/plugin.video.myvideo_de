445d6345cc7ff50283243ed0bbcddb5f0471ddb5
dersphere <sphere@dersphere.de>
2013-04-26 22:23:25 +0200

    moved tests to separate file

--- resources/lib/scraper.py
+++ resources/lib/scraper.py
@@ -678,61 +678,6 @@ def log(msg):
     print('MyVideo.de scraper: %s' % msg)
 
 
-def test():
-    path_list = (
-        'Top_100',
-        '/Top_100/Top_100_Charts',
-        '/Top_100/Top_100_Single_Charts',
-        '/Top_100/Top_100_Serien',
-        '/Top_100/Top_100_Entertainment',
-        '/Top_100/Top_100_Musik_Clips',
-        '/Top_100/Top_100_Filme',
-        'Videos_A-Z',
-        'Videos_A-Z/Videos_in_Kategorien',
-        '/Videos_A-Z?lpage=2&searchWord=&searchOrder=1',
-        'Serien',
-        'Top_100/Top_100_Serien',
-        '/Serien/Alle_Serien_A-Z',
-        '/channel/17-Meter',
-        '/iframe.php?lpage=2&function=mv_charts&action=highlight_clips&page=9669699&tab=1',
-        '/channel/ahnungslos-prosieben',
-        '/iframe.php?lpage=2&function=mv_charts&action=full_episodes&page=9150775&tab=1',
-        '/channel/videovalis_albert-sagt-natur',
-        '/channel/zwei-bei-kallwass',
-        '/Serien/ProSieben',
-        '/Serien/Sony_Retro',
-        '/Serien/Welt_der_Wunder',
-        '/Serien/Weitere_Serien',
-        '/channel/30-minuten-deutschland',
-        '/channel/we-love-mma',
-        '/iframe.php?lpage=2&function=mv_charts&action=full_episodes&page=9244249&tab=1',
-        'Filme',
-        'Top_100/Top_100_Filme',
-        '/Videos_A-Z?searchChannelID=369&searchChannel=Film',
-        '/Videos_A-Z?lpage=2&searchWord=&searchChannelID=369&searchChannel=Film&searchOrder=1',
-        '/Filme/Comedy',
-        '/Filme/Horror',
-        '/Filme/Thriller',
-        '/iframe.php?lpage=2&function=mv_success_box&action=filme_video_list&searchGroup=74590&searchOrder=1',
-        '/Filme/Konzerte',
-        'Musik',
-        '/Musik/Neue_Musik_Videos',
-        '/Musik/Pop',
-        '/iframe.php?lpage=2&function=mv_charts&action=music_videos&page=music_pop&tab=0',
-        '/Musik/Musik_K%C3%Bcnstler',
-        '/Musik/Musik_K%C3%BCnstler?lpage=4',
-        '/channel/daughtry-official',
-    )
-    for path in path_list:
-        scraper = BaseScraper.choose_scraper(path)
-        tree = requester.get_tree(MAIN_URL + path)
-        if not scraper:
-            raise Exception('No scraper found for path: %s' % path)
-        items, next_page, prev_page = scraper.parse(tree)
-        items = [i for i in items]
-        if not items:
-            raise Exception('No items found for path: %s' % path)
-
 requester = SessionRequester()
 
 if __name__ == '__main__':
