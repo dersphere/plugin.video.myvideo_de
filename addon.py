86b0f653cd1ff53af61af721162e9f11caadd58f
dersphere <sphere@dersphere.de>
2013-08-11 13:01:16 +0200

    update year in copyright

--- addon.py
+++ addon.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#     Copyright (C) 2012 Tristan Fischer (sphere@dersphere.de)
+#     Copyright (C) 2013 Tristan Fischer (sphere@dersphere.de)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
